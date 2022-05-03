f003e2eec598d9caf3b8ea8ef3b131d53f5a33e2
vingadorv <vinifranca13@gmail.com>
2022-05-02 22:00:12 -0300

    Database Context done

---
+++ ControleFinanceiro/Data/FinancialContext.cs
@@ -0,0 +1,13 @@
+﻿using ControleFinanceiro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro.Data
+{
+    public class FinancialContext : DbContext
+    {
+        public FinancialContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Expenditure> Expenditure { get; set; }
+        public DbSet<Revenue> Revenue { get; set; }
+    }
+}
---
+++ ControleFinanceiro/Startup.cs
@@ -0,0 +1,43 @@
+﻿using ControleFinanceiro.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<FinancialContext>(opt => opt.UseSqlServer
+                (Configuration.GetConnectionString("SQLConnection")));
+
+            services.AddControllers();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
